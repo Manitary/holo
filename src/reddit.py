0180ef476dc42943bc8c01d4b97419c54a8b84a6
Manitary <pak.ska@gmail.com>
2023-05-30 23:15:04 +0100

    Start refactoring of reddit
    Create new classes and methods
    Don't touch anything else until refactoring the files importing it

--- src/reddit.py
+++ src/reddit.py
@@ -1,5 +1,9 @@
-from logging import debug, info, warning, error, exception
+import logging
 import praw
+from praw.models import Submission
+from config import Config
+
+logger = logging.getLogger(__name__)
 
 # Initialization
 
@@ -7,14 +11,16 @@ _r = None
 _config = None
 
 
-def init_reddit(config):
+	
+
+def init_reddit(config: Config) -> None:
 	global _config
 	_config = config
 
 
 def _connect_reddit():
 	if _config is None:
-		error("Can't connect to reddit without a config")
+		logger.error("Can't connect to reddit without a config")
 		return None
 
 	return praw.Reddit(
@@ -32,6 +38,62 @@ def _ensure_connection():
 	if _r is None:
 		_r = _connect_reddit()
 	return _r is not None
+
+class RedditHolo:
+	def __init__(self, config: Config) -> None:
+		self._reddit: praw.Reddit = praw.Reddit(
+			client_id=config.r_oauth_key,
+			client_secret=config.r_oauth_secret,
+			username=config.r_username,
+			password=config.r_password,
+			user_agent=config.useragent,
+			check_for_updates=False,
+		)
+		self._config: Config = config
+
+	def submit_text_post(self, subreddit: str, title: str, body: str) -> Submission | None:
+		try:
+			logger.info("Checking availability of flair %s", self._config.post_flair_id)
+			flair_ids: dict[str, str | bool] = [
+				ft["flair_template_id"]
+				for ft in self._reddit.subreddit(subreddit).flair.link_templates.user_selectable() # type: ignore
+			]
+			if self._config.post_flair_id in flair_ids:
+				flair_id, flair_text = self._config.post_flair_id, self._config.post_flair_text
+			else:
+				logger.warning("Flair not selectable, flairing will be disabled")
+				flair_id, flair_text = None, None
+
+			logger.info("Submitting post to %s", subreddit)
+			new_post: Submission = self._reddit.subreddit(subreddit).submit( # type: ignore
+				title,
+				selftext=body,
+				flair_id=flair_id,
+				flair_text=flair_text,
+				send_replies=False,
+			)
+			return new_post # type: ignore
+		except:
+			logger.exception("Failed to submit text post")
+			return None
+
+	def edit_text_post(self, url: str, body: str) -> Submission | None:
+		try:
+			logger.info("Editing post %s", url)
+			post: Submission = get_text_post(url) # type: ignore
+			post.edit(body=body) # type: ignore
+			return post
+		except:
+			logger.exception("Failed to submit text post")
+			return None
+
+	def get_text_post(self, url: str) -> Submission | None:
+		try:
+			new_post: Submission = self._reddit.submission(url=url) # type: ignore
+			return new_post # type: ignore
+		except:
+			logger.exception("Failed to retrieve text post")
+			return None
 
 
 # Thing doing
@@ -109,5 +171,6 @@ def get_text_post(url):
 # Utilities
 
 
-def get_shortlink_from_id(id):
-	return "http://redd.it/{}".format(id)
+def get_shortlink_from_id(submission_id: str) -> str:
+	# TODO Verify if it can be changed to https without breaking anything
+	return f"http://redd.it/{submission_id}"
