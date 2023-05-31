4266a41654d9c8df8d9fccf1cdee137af345707d
Manitary <pak.ska@gmail.com>
2023-05-31 01:04:10 +0100

    Refactor models
    A few class constructor were slightly changed
    Things may break down the line depending on implementation details

--- src/data/models.py
+++ src/data/models.py
@@ -1,9 +1,11 @@
 from datetime import datetime
+from dataclasses import dataclass, field
 import enum
 import copy
+from typing import Self
 
 
-class ShowType(enum.Enum):
+class ShowType(enum.IntEnum):
 	UNKNOWN = 0
 	TV = 1
 	MOVIE = 2
@@ -16,59 +18,46 @@ def str_to_showtype(string: str) -> ShowType:
 	except KeyError:
 		return ShowType.UNKNOWN
 
-
 class DbEqMixin:
-	def __eq__(self, other):
+	id: int
+
+	def __eq__(self, other: Self) -> bool:
 		return self.id == other.id
 
-	def __ne__(self, other):
+	def __ne__(self, other: Self) -> bool:
 		return self.id != other.id
 
-	def __hash__(self):
+	def __hash__(self) -> int:
 		return hash(self.id)
 
-
+@dataclass
 class Show(DbEqMixin):
-	def __init__(
-		self,
-		id,
-		name,
-		name_en,
-		length,
-		show_type,
-		has_source,
-		is_nsfw,
-		enabled,
-		delayed,
-	):
-		# Note: arguments are order-sensitive
-		# Should probably be moved to keyword args, but not sure the best method on the database side
-		self.id = id
-		self.name = name
-		self.name_en = name_en
-		self.length = length
-		self.type = show_type
-		self.has_source = has_source == 1
-		self.is_nsfw = is_nsfw == 1
-		self.enabled = enabled
-		self.delayed = delayed
+	# Note: arguments are order-sensitive
+	id: int
+	name: str
+	name_en: str
+	length: int
+	type: int
+	has_source: int
+	is_nsfw: int
+	enabled: int
+	delayed: int
+	_aliases: list[str] = field(default_factory=list)
 
 	@property
-	def aliases(self):
-		return self._aliases if hasattr(self, "_aliases") else []
+	def aliases(self) -> list[str]:
+		return self._aliases
 
 	@aliases.setter
-	def aliases(self, names):
+	def aliases(self, names: list[str]) -> None:
 		self._aliases = names
 
-	def __str__(self):
-		return "Show: {} (id={}, type={}, len={})".format(
-			self.name, self.id, self.type, self.length
-		)
+	def __str__(self) -> str:
+		return f"Show: {self.name} (id={self.id}, type={self.type}, len={self.length})"
 
-
+@dataclass
 class Episode:
-	def __init__(self, number, name, link, date):
+	def __init__(self, number: int, name: str, link: str, date: datetime | list[int]) -> None:
 		# Note: arguments are order-sensitive
 		self.number = number
 		self.name = name  # Not stored in database
@@ -76,40 +65,36 @@ class Episode:
 		if isinstance(date, datetime):
 			self.date = date
 		elif date:
-			self.date = datetime(*date[:6])
+			self.date = datetime(*date[:6]) # type: ignore
 
-	def __str__(self):
-		return "Episode: {} | Episode {}, {} ({})".format(
-			self.date, self.number, self.name, self.link
-		)
+	def __str__(self) -> str:
+		return f"Episode: {self.date} | Episode {self.number}, {self.name} ({self.link})"
 
 	@property
-	def is_live(self, local=False):
+	def is_live(self, local: bool = False) -> bool:
 		now = datetime.now() if local else datetime.utcnow()
 		return now >= self.date
 
-
+@dataclass
 class EpisodeScore:
-	def __init__(self, show_id, episode, site_id, score):
-		self.show_id = show_id
-		self.episode = episode
-		self.site_id = site_id
-		self.score = score
+	show_id: int
+	episode: int
+	site_id: int
+	score: float
 
-
+@dataclass
 class Service(DbEqMixin):
-	def __init__(self, id, key, name, enabled, use_in_post):
-		# Note: arguments are order-sensitive
-		self.id = id
-		self.key = key
-		self.name = name
-		self.enabled = enabled == 1
-		self.use_in_post = use_in_post == 1
+	# Note: arguments are order-sensitive
+	id: int
+	key: str
+	name: str
+	enabled: int
+	use_in_post: int
 
-	def __str__(self):
-		return "Service: {} ({})".format(self.key, self.id)
+	def __str__(self) -> str:
+		return f"Service: {self.key} ({self.id})"
 
-
+@dataclass
 class Stream(DbEqMixin):
 	"""
 	remote_offset: relative to a start episode of 1
@@ -119,88 +104,68 @@ class Stream(DbEqMixin):
 			If a show should be displayed with higher numbering (ex. continuing after a split cour), display_offset should be positive.
 			If a show should be numbered lower than 1 (ex. 0), display_offset should be negative.
 	"""
+	# Note: arguments are order-sensitive
+	id: int
+	service: int
+	show: int
+	show_id: str | None
+	show_key: str
+	name: str
+	remote_offset: int = 0
+	display_offset: int = 0
+	active: int = 1
 
-	def __init__(
-		self,
-		id,
-		service,
-		show,
-		show_id,
-		show_key,
-		name,
-		remote_offset,
-		display_offset,
-		active,
-	):
-		# Note: arguments are order-sensitive
-		self.id = id
-		self.service = service
-		self.show = show
-		self.show_id = show_id
-		self.show_key = show_key
-		self.name = name
-		self.remote_offset = remote_offset
-		self.display_offset = display_offset
-		self.active = active
-
-	def __str__(self):
-		return "Stream: {} ({}@{}), {} {}".format(
-			self.show,
-			self.show_key,
-			self.service,
-			self.remote_offset,
-			self.display_offset,
-		)
+	def __str__(self) -> str:
+		return f"Stream: {self.show} ({self.show_key}@{self.service}), {self.remote_offset} {self.display_offset}"
 
 	@classmethod
-	def from_show(cls, show):
-		return Stream(-show.id, -1, show, show.id, show.name, show.name, 0, 0, 1)
+	def from_show(cls, show: Show) -> Self:
+		return Stream(-show.id, -1, show.id, None, show.name, show.name)
 
-	def to_internal_episode(self, episode):
+	def to_internal_episode(self, episode: Episode) -> Episode:
+		# ? Seems unused? Why a shallow copy instead of altering directly?
 		e = copy.copy(episode)
 		e.number -= self.remote_offset
 		return e
 
-	def to_display_episode(self, episode):
+	def to_display_episode(self, episode: Episode) -> Episode:
+		# ? Seems unused? Why a shallow copy instead of altering directly?
 		e = copy.copy(episode)
 		e.number += self.display_offset
 		return e
 
-
+@dataclass
 class LinkSite(DbEqMixin):
-	def __init__(self, id, key, name, enabled):
-		# Note: arguments are order-sensitive
-		self.id = id
-		self.key = key
-		self.name = name
-		self.enabled = enabled == 1
+	# Note: arguments are order-sensitive
+	id: int
+	key: str
+	name: str
+	enabled: int
+	
+	def __str__(self) -> str:
+		return f"Link site: {self.key} ({self.id})"
 
-	def __str__(self):
-		return "Link site: {} ({})".format(self.key, self.id, self.enabled)
-
-
+@dataclass
 class Link:
-	def __init__(self, site, show, site_key):
-		# Note: arguments are order-sensitive
-		self.site = site
-		self.show = show
-		self.site_key = site_key
+	# Note: arguments are order-sensitive
+	site: str
+	show: str
+	site_key: str
+	
+	def __str__(self) -> str:
+		return f"Link: {self.site_key}@{self.site}, show={self.show}"
 
-	def __str__(self):
-		return "Link: {}@{}, show={}".format(self.site_key, self.site, self.show)
-
-
+@dataclass
 class PollSite(DbEqMixin):
-	def __init__(self, id, key):
-		self.id = id
-		self.key = key
-
-	def __str__(self):
+	id: int
+	key: str
+	
+	def __str__(self) -> str:
 		return f"Poll site: {self.key}"
 
 
 class Poll:
-	def __init__(self, show_id, episode, service, id, date, score):
+	def __init__(self, show_id: int, episode: int, service: int, id: str, date: datetime | str | int, score: float | None = None) -> None:
 		self.show_id = show_id
 		self.episode = episode
 		self.service_id = service
@@ -212,56 +177,40 @@ class Poll:
 		self.score = score
 
 	@property
-	def has_score(self):
+	def has_score(self) -> bool:
 		return self.score is not None
 
-	def __str__(self):
+	def __str__(self) -> str:
 		return f"Poll {self.show_id}/{self.episode} (Score {self.score})"
 
-
+@dataclass
 class LiteStream:
-	def __init__(self, show, service, service_name, url):
-		# Note: arguments are order-sensitive
-		self.show = show
-		self.service = service
-		self.service_name = service_name
-		self.url = url
+	# Note: arguments are order-sensitive
+	show: int
+	service: str
+	service_name: str
+	url: str
 
-	def __str__(self):
+	def __str__(self) -> str:
 		return f"LiteStream: {self.service}|{self.service_name}, show={self.show}, url={self.url}"
 
-
+@dataclass
 class UnprocessedShow:
-	def __init__(
-		self,
-		site_key,
-		show_key,
-		name,
-		name_en,
-		more_names,
-		show_type,
-		episode_count,
-		has_source,
-		is_nsfw,
-	):
-		self.site_key = site_key
-		self.show_key = show_key
-		self.name = name
-		self.name_en = name_en
-		self.more_names = more_names
-		self.show_type = show_type
-		self.episode_count = episode_count
-		self.has_source = has_source
-		self.is_nsfw = is_nsfw
+	site_key: str
+	show_key: str
+	name: str
+	name_en: str
+	more_names: list[str]
+	show_type: int
+	episode_count: int
+	has_source: int
+	is_nsfw: int
 
-
+@dataclass
 class UnprocessedStream:
-	def __init__(
-		self, service_key, show_key, show_id, name, remote_offset, display_offset
-	):
-		self.service_key = service_key
-		self.show_key = show_key
-		self.show_id = show_id
-		self.name = name
-		self.remote_offset = remote_offset
-		self.display_offset = display_offset
+	service_key: str
+	show_key: str
+	show_id: int
+	name: str
+	remote_offset: int
+	display_offset: int
