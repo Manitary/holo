9fd6171d6288706e0b2efec25011ae1355fcaa7b
Manitary <pak.ska@gmail.com>
2023-07-22 23:04:37 +0100

    Update db schema
    * Remove unnecessary ``unique``
    * Add cascade updates of foreign keys
    * Add some default values
    * Add some unique constraint and conflict behaviour

--- src/data/create_tables.sql
+++ src/data/create_tables.sql
@@ -1,19 +1,19 @@
 CREATE TABLE IF NOT EXISTS ShowTypes (
-    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     key TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Shows (
-    id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     name        TEXT NOT NULL,
     name_en     TEXT,
-    length      INTEGER,
+    length      INTEGER DEFAULT 0,
     type        INTEGER NOT NULL,
     has_source  INTEGER NOT NULL DEFAULT 0,
     is_nsfw     INTEGER NOT NULL DEFAULT 0,
     enabled     INTEGER NOT NULL DEFAULT 1,
     delayed     INTEGER NOT NULL DEFAULT 0,
-    FOREIGN KEY(type) REFERENCES ShowTypes(id)
+    FOREIGN KEY(type) REFERENCES ShowTypes(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS ShowNames (
@@ -24,12 +24,12 @@ CREATE TABLE IF NOT EXISTS ShowNames (
 CREATE TABLE IF NOT EXISTS Aliases (
     show    INTEGER NOT NULL,
     alias   TEXT NOT NULL,
-    FOREIGN KEY(show) REFERENCES Shows(id),
-    UNIQUE(show, alias) ON CONFLICT IGNORE
+    UNIQUE(show, alias) ON CONFLICT IGNORE,
+    FOREIGN KEY(show) REFERENCES Shows(id)
 );
 
 CREATE TABLE IF NOT EXISTS Services (
-    id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     key         TEXT NOT NULL UNIQUE,
     name        TEXT NOT NULL,
     enabled     INTEGER NOT NULL DEFAULT 0,
@@ -37,17 +37,18 @@ CREATE TABLE IF NOT EXISTS Services (
 );
 
 CREATE TABLE IF NOT EXISTS Streams (
-    id              INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    service         TEXT NOT NULL,
-    show            INTEGER,
+    id              INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    service         INTEGER NOT NULL,
+    show            INTEGER NOT NULL,
     show_id         TEXT,
     show_key        TEXT NOT NULL,
     name            TEXT,
     remote_offset   INTEGER NOT NULL DEFAULT 0,
     display_offset  INTEGER NOT NULL DEFAULT 0,
     active          INTEGER NOT NULL DEFAULT 1,
-    FOREIGN KEY(service) REFERENCES Services(id),
-    FOREIGN KEY(show) REFERENCES Shows(id)
+    UNIQUE(service, show),
+    FOREIGN KEY(service) REFERENCES Services(id) ON UPDATE CASCADE,
+    FOREIGN KEY(show) REFERENCES Shows(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Episodes (
@@ -55,11 +56,11 @@ CREATE TABLE IF NOT EXISTS Episodes (
     episode     INTEGER NOT NULL,
     post_url    TEXT,
     UNIQUE(show, episode) ON CONFLICT REPLACE,
-    FOREIGN KEY(show) REFERENCES Shows(id)
+    FOREIGN KEY(show) REFERENCES Shows(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS LinkSites (
-    id      INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    id      INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     key     TEXT NOT NULL UNIQUE,
     name    TEXT NOT NULL,
     enabled INTEGER NOT NULL DEFAULT 1
@@ -69,8 +70,9 @@ CREATE TABLE IF NOT EXISTS Links (
     show        INTEGER NOT NULL,
     site        INTEGER NOT NULL,
     site_key    TEXT NOT NULL,
-    FOREIGN KEY(site) REFERENCES LinkSites(id),
-    FOREIGN KEY(show) REFERENCES Shows(id)
+    UNIQUE(show, site) ON CONFLICT IGNORE,
+    FOREIGN KEY(site) REFERENCES LinkSites(id) ON UPDATE CASCADE,
+    FOREIGN KEY(show) REFERENCES Shows(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Scores (
@@ -78,21 +80,22 @@ CREATE TABLE IF NOT EXISTS Scores (
     episode INTEGER NOT NULL,
     site    INTEGER NOT NULL,
     score   REAL NOT NULL,
-    FOREIGN KEY(show) REFERENCES Shows(id),
-    FOREIGN KEY(site) REFERENCES LinkSites(id)
+    UNIQUE(show, episode, site) ON CONFLICT REPLACE,
+    FOREIGN KEY(show) REFERENCES Shows(id) ON UPDATE CASCADE,
+    FOREIGN KEY(site) REFERENCES LinkSites(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS LiteStreams (
-    show           INTEGER NOT NULL,
-    service         TEXT,
+    show            INTEGER NOT NULL,
+    service         TEXT NOT NULL,
     service_name    TEXT NOT NULL,
     url             TEXT,
     UNIQUE(show, service) ON CONFLICT REPLACE,
-    FOREIGN KEY(show) REFERENCES Shows(id)
+    FOREIGN KEY(show) REFERENCES Shows(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS PollSites (
-    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     key TEXT NOT NULL UNIQUE
 );
 
@@ -103,7 +106,7 @@ CREATE TABLE IF NOT EXISTS Polls (
     poll_id         TEXT NOT NULL,
     timestamp       INTEGER NOT NULL,
     score           REAL,
-    FOREIGN KEY(show) REFERENCES Shows(id),
-    FOREIGN KEY(poll_service) REFERENCES PollSites(id),
-    UNIQUE(show, episode) ON CONFLICT REPLACE
+    UNIQUE(show, episode, poll_service) ON CONFLICT REPLACE,
+    FOREIGN KEY(show) REFERENCES Shows(id) ON UPDATE CASCADE,
+    FOREIGN KEY(poll_service) REFERENCES PollSites(id) ON UPDATE CASCADE
 );
